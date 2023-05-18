d7fd1e13865b5e0ceabd2bca5db480d28ec1f1e2
Alexey Zapparov <alexey@zapparov.com>
2023-05-18 16:29:29 +0200

    Backport synchronization removal from v3.1.0

--- lib/sidekiq/pauzer/queues.rb
+++ lib/sidekiq/pauzer/queues.rb
@@ -14,8 +14,7 @@ module Sidekiq
 
       # @param config [Config]
       def initialize(config)
-        @mutex     = Mutex.new
-        @names     = []
+        @names     = Concurrent::Array.new
         @redis_key = config.redis_key
         @refresher = initialize_refresher(config.refresh_rate)
       end
@@ -23,7 +22,7 @@ module Sidekiq
       def each(&block)
         return to_enum __method__ unless block
 
-        @mutex.synchronize { @names.dup }.each(&block)
+        @names.dup.each(&block)
 
         self
       end
@@ -73,11 +72,7 @@ module Sidekiq
       end
 
       def refresh
-        @mutex.synchronize do
-          names = Sidekiq.redis { |conn| Adapters[conn].list(conn, @redis_key) }
-
-          @names.replace(names)
-        end
+        @names.replace(Sidekiq.redis { |conn| Adapters[conn].list(conn, @redis_key) })
 
         self
       end
